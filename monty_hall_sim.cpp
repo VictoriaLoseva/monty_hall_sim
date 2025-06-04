3de56ea65019ec5d28946127ae223820745e555b
Victoria Loseva <vloseva@gmail.com>
2025-06-04 12:03:49 -0700

    more checks for failure cases, refactoring.

--- monty_hall_sim.cpp
+++ monty_hall_sim.cpp
@@ -1,85 +1,139 @@
 /* 
 ** Monty Hall Problem Simulator
 ** 
-** Input:  NUM_SIMS, an integer representing the number of trials. 
-** Output: N/A. Program will print results to the output stream. 
+** Input:  num_trials, integers greater than 1. 
+** Output: 0 for successful execution. 
+**         1 for input error. 
+**         2 for memory error. 
+**         Program will print results to the output stream.
 ** 
-** Half of NUM_SIMS trials will use the 'switch' strategy and the other half 
-** will use the 'stay' strategy. The trials are completely independent of each 
-** other. 
+** The program will run num_trials simulations of one strategy and num_trials
+** of the other strategy. 
+** The simulations are completely statistically independent. 
 ** 
 ** For this reason, it is best to use a large number of trials to get 
-** significant results. 
+** results consistent with the theoretical. 
 */
 
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <iomanip>  
 
 #define SWITCH 0 
 #define KEEP 1
 
+int parse_num_trials(const int& argc, const char* const argv[], int* num_trials) {
+    for(int i = 1; i < argc; i++) {
+        try {
+            num_trials[i-1] = std::stoi(argv[i]);
+        }
+        catch (std::invalid_argument e) {
+            std::cerr << "monty_hall_sim: invalid argument, num_trials needs to be a number greater than 1." << std::endl; 
+            std::cerr << "monty_hall_sim: Got " << argv[i] << " instead." << std::endl;
+            std::cerr << e.what() << std::endl;
+            return 1; 
+        }
+        catch(std::out_of_range e) {
+            std::cerr << "monty_hall_sim: invalid argument, num_trials out of range for int." << std::endl; 
+            std::cerr << "monty_hall_sim: Got " << argv[i] << "." << std::endl;
+            std::cerr << e.what() << std::endl;
+            return 1; 
+        }
+
+        if(num_trials[i-1] < 1) {
+            std::cerr << "monty_hall_sim: invalid argument, num_trials needs to be a number greater than 1. " << std::endl;
+            std::cerr << "monty_hall_sim: Got " << argv[i] << " instead." << std::endl;
+            return 1;
+        }
+
+    }
+    return 0; 
+}
+
+void run_sims(const int num_sims, const int* const num_trials, int* const wins) {
+        for(int sim = 0; sim < num_sims; sim++) {
+            for(int i = 0; i < num_trials[sim]*2; i++) {
+                // Dealer places the car
+                bool doors[3] = {false, false, false};
+                int car_location = std::rand() % 3;
+                doors[car_location] = true;
+
+                // Player makes initial choice 
+                int choice = std::rand() % 3;
+                
+                // Dealer reveals one of the losing doors
+                // Always the one to left of the car. This should not affect the randomness of simulation 
+                int revealed = (car_location + 1) % 3; 
+                if(revealed == choice) revealed = (car_location + 2 ) % 3;
+
+                // Player makes next choice; half the time, they switch
+                int strategy = i % 2;
+                if(strategy == SWITCH) choice = 3 - choice - revealed; 
+
+                if(doors[choice])
+                    wins[sim*2 + strategy]++; 
+            }
+    }
+}
+
+void print_results(const int num_sims, const int* const num_trials, const int* const wins){
+    std::cout  << std::endl;
+    std::cout << "          MONTY HALL SIM RESULTS           " << std::endl; 
+    std::cout << "===========================================" << std::endl;
+    std::cout << "# of trials | SWITCH, % wins | STAY, % wins" << std::endl; 
+    for(int sim = 0; sim < num_sims; sim++) {
+        std::cout << "------------|----------------|-------------" << std::endl;
+        int len_trials_number = std::to_string(num_trials[sim]).length();
+        std::string padding = std::string((12 - len_trials_number)/2, ' '); 
+        std::string extra = std::string(len_trials_number%2, ' ');
+        std::cout << std::left << padding << extra << num_trials[sim] << padding << "|";
+        std::cout << std::setprecision(2) << std::fixed; 
+        padding = std::string((16 - 5)/2, ' '); 
+        std::cout << padding << float(wins[sim*2 + SWITCH]) / (num_trials[sim]) * 100 << "%" << padding << "|";
+        padding = std::string((13 - 5)/2, ' '); 
+        std::cout << padding << float(wins[sim*2 + KEEP]) / (num_trials[sim])*100 << "%" << std::endl;
+    }
+    std::cout << std::endl; 
+}
+
 
 int main(int argc, char* argv[]) {
-    if(argc != 2) {
-        std::cerr << "ERROR: Need exactly one argument, the number of trials to simulate" << std::endl; 
-        std::cerr << "Got " << argc << " instead" << std::endl; 
+
+    if(argc < 2) {
+        std::cout << "monty_hall_sim: need at least one argument, number of trials, to run." << std::endl; 
         return 1; 
     }
 
-    int NUM_SIMS;
-
+    int num_sims = argc - 1; 
+    int* num_trials;
+    
     try {
-        NUM_SIMS = std::stoi(argv[1]);
+        num_trials = new int[num_sims];
     }
-    catch (std::invalid_argument e) {
-        std::cerr << "ERROR: Argument needs to be a number." << std::endl; 
-        std::cerr << e.what() << std::endl;
-        return 1; 
+    catch (std::bad_alloc e) {
+        std::cerr << "monty_hall_sim: not enough memory to allocate array for simulation information." << std::endl;
+        return 2;
     }
 
-    if(NUM_SIMS < 2) {
-        std::cerr << "ERROR: Need to simulate at least two trials. " << std::endl;
+    if(parse_num_trials(argc, argv, num_trials) != 0)
         return 1;
+
+    
+    int* wins;
+    try {
+        wins = new int[num_sims * 2];
     }
-
-    if(NUM_SIMS < 200) {
-        std::cerr << "Warning: small amount of trials will not produce consistent results." << std::endl;
+    catch (std::bad_alloc e) {
+        std::cerr << "monty_hall_sim: not enough memory to allocate array for results." << std::endl;
+        return 2;
     }
+    
+    std::srand(std::time(0)); 
+    
+    run_sims(num_sims, num_trials, wins);
 
-    bool doors[3] = {false, false, false};
-    float wins[2] = {0};
-
-    std::srand(std::time(0));
-
-    for(int i = 0; i < NUM_SIMS; i++) {
-        int car_location = std::rand() % 3;
-
-        // Dealer places the car
-        doors[car_location] = true;
-
-        // Player makes initial choice 
-        int choice = std::rand() % 3;
-        
-        // Dealer reveals one of the losing doors
-        // Always the one to left of the car. This should not affect randomness of simulation 
-        int revealed = (car_location + 1) % 3; 
-        if(revealed == choice) revealed = (car_location + 2 ) % 3;
-
-        // Player makes next choice; half the time, they switch
-        int strategy = i % 2;
-        if(strategy == SWITCH) choice = 3 - choice - revealed; 
-
-        if(doors[choice])
-            wins[strategy]++; 
-        
-        //Reset state for next iteration 
-        doors[car_location] = false;
-    }
-
-    std::cout << "Trials: " << NUM_SIMS << std::endl; 
-    std::cout << "   Switch: " << (wins[SWITCH]) / (NUM_SIMS / 2 + NUM_SIMS % 2) << std::endl; 
-    std::cout << "   Stay:   " << (wins[KEEP]) / (NUM_SIMS / 2) << std::endl; 
+    print_results(num_sims, num_trials, wins);
 
     return 0; 
 }
